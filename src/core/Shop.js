a2b5e07b513a57c11d1a6da6c67082b40fa20436
Tanmay Jain <tanmayjain53@gmail.com>
2019-12-03 23:57:10 +0530

    Added price filter in shop

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,10 +3,18 @@ import Layout from "./Layout";
 import Card from "./Card";
 import {getCategories} from "./apiCore";
 import Checkbox from "./Checkbox";
+import Radiobox from "./Radiobox";
+import {prices} from "./fixdPrices"
 
 
 const Shop = () =>{
 
+    const [myFilters,setMyFilters] = useState({
+        filters:{
+            category:[],
+            price:[]
+        }
+    });
     const [categories,setCategories] = useState([]);
     const [error,setError] = useState(false);
 
@@ -24,17 +32,43 @@ const Shop = () =>{
         init();
     },[])
 
+    const handleFilters = (filters,filterBy)=>{
+        const newFilters = {...myFilters};
+        newFilters.filters[filterBy] = filters;
+
+        if(filterBy=="price"){
+            let priceValues = handlePrice(filters)
+            newFilters.filters[filterBy] = priceValues;
+        }
+        setMyFilters(newFilters);
+    }
+
+    const handlePrice = value =>{
+        const data = prices;
+        let array = [];
+        for(let key in data){
+            if(data[key]._id==value){
+                array=data[key].array;
+            }
+        }
+        return array;
+    }
+
     return (
         <Layout title="Shop Page" className="container-fluid">
             <div className="row">
                 <div className="col-4">
                     <h4>Filter by categories</h4>
                     <ul>
-                        <Checkbox categories={categories}/>
+                        <Checkbox categories={categories} handleFilters={filters=> handleFilters(filters,"category")}/>
                     </ul>
+                    <h4>Filter by prices</h4>
+                    <div>
+                        <Radiobox prices={prices} handleFilters={filters=> handleFilters(filters,"price")}/>
+                    </div>
                 </div>
                 <div className="col-8">
-                    right sidebar
+                    {JSON.stringify(myFilters)}
                 </div>
             </div>
         </Layout>
