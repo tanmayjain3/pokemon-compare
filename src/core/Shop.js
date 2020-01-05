8842cf08046eb1127cfc859f4b2e7f776c856eb5
Tanmay Jain <tanmayjain53@gmail.com>
2020-01-05 21:44:06 +0530

    Added shop component

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from "react";
 import Layout from "./Layout";
-import {getCategories} from "./apiCore";
+import {getCategories, getFilteredProducts} from "./apiCore";
 import Checkbox from "./Checkbox";
 import Radiobox from "./Radiobox";
 import {prices} from "./fixdPrices"
+import Card from "./Card";
 
 
 const Shop = () =>{
@@ -16,6 +17,11 @@ const Shop = () =>{
     });
     const [categories,setCategories] = useState([]);
     const [error,setError] = useState(false);
+    const [limit,setLimit] = useState(6);
+    const [skip,setSkip] = useState(0);
+    const [size,setSize] = useState(0);
+    const [filteredResults,setFilteredResults] = useState([]);
+
     console.log(error);
     const init =() =>{
         getCategories().then(data=>{
@@ -27,8 +33,42 @@ const Shop = () =>{
         })
     }
 
+    const loadFilteredResults = (newFilters) =>{
+        getFilteredProducts(skip,limit,newFilters).then(data=>{
+            if(data.error){
+                setError(data.error);
+            } else{
+                setFilteredResults(data.data);
+                setSize(data.size);
+                setSkip(0);
+            }
+        })
+    }
+
+    const loadMore = () =>{
+        let toSkip = skip + limit;
+        getFilteredProducts(toSkip,limit,myFilters.filters).then(data=>{
+            if(data.error){
+                setError(data.error);
+            } else{
+                setFilteredResults([...filteredResults,...data.data]);
+                setSize(data.size);
+                setSkip(0);
+            }
+        })
+    }
+
+    const loadMoreButton = () =>{
+        return (
+                size>0 && size>=limit && (
+                    <button onClick={loadMore} className= "btn btn-warning mb-5">Load More</button>
+                )
+        )
+    }
+    
     useEffect(()=>{
         init();
+        loadFilteredResults(myFilters.filters)
     },[])
 
     const handleFilters = (filters,filterBy)=>{
@@ -39,6 +79,8 @@ const Shop = () =>{
             let priceValues = handlePrice(filters)
             newFilters.filters[filterBy] = priceValues;
         }
+
+        loadFilteredResults(myFilters.filters);
         setMyFilters(newFilters);
     }
 
@@ -46,7 +88,7 @@ const Shop = () =>{
         const data = prices;
         let array = [];
         for(let key in data){
-            if(data[key]._id===value){
+            if(data[key]._id===parseInt(value)){
                 array=data[key].array;
             }
         }
@@ -67,7 +109,15 @@ const Shop = () =>{
                     </div>
                 </div>
                 <div className="col-8">
-                    {JSON.stringify(myFilters)}
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                    {
+                        filteredResults.map((product,i)=>(
+                                <Card key={i} product={product}/>
+                        ))}
+                        </div>
+                        <hr/>
+                        {loadMoreButton()}
                 </div>
             </div>
         </Layout>
